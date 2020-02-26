0964a894e44a10d15459d6fe2577d43e851cbdc7
keniklee <kenik@naver.com>
2020-02-26 17:33:50 +0900

    days01 package add

---
+++ JavaPro/src/days01/Ex01.java
@@ -0,0 +1,16 @@
+package days01;
+
+
+/**
+ * @author SIST163 Alt + Shift + J 
+ *
+ */
+public class Ex01 {
+
+	public static void main(String[] args) {
+		// syso Ctrl + Space
+		System.out.println("Java Programming...");
+
+	}
+
+}
